27be2c0035ee13df49c26315d799f22f1269b2d4
srkarvhhs <60476478+srkarvhhs@users.noreply.github.com>
2020-03-15 12:57:16 -0500

    Create EndofFile.java

---
+++ EndofFile.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        int n = 0;
+   Scanner scan = new Scanner(System.in);
+  while(scan.hasNext() == true){
+      n++;
+      String s = scan.nextLine();
+      System.out.println(n + " " +s);
+  }
+    }
+}
